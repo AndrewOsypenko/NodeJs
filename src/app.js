a7ad6370bf663a7083945c15bd91cbd9865be6c8
Ozzy <osp.andrew@gmail.com>
2020-11-01 21:22:30 +0200

    Add errorHandler

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import express from 'express';
-import "./db/mongoose.js";
-import  taskRouter from './router/task.js'
 import cors from 'cors'
+import './db/mongoose.js';
+import  taskRouter from './router/task.js'
+import  errorHandler from './middleware/errorHandler.js'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(taskRouter);
-app.use((err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err);
-    }
-    res.status(500).json({error: err})
-});
+app.use(errorHandler);
 export default app;
