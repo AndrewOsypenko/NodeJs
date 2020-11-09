558c8bfa9600abede03894067b5808b2cdcacf63
Ozzy <osp.andrew@gmail.com>
2020-11-09 21:11:22 +0200

    yarn init

---
+++ src/models/task.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const Task = mongoose.model('Todos', taskSchema );
+
+export default Task;
